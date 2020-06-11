deb7946f87d8ab3f1f009f47f9eb1863a871eeff
王孝敏 <wangxiaomin@lattebank.com>
2020-06-11 18:29:03 +0800

    init

--- blog/blogdjango/blogdjango/__init__.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-# from django.core import signals
-# from django.db import close_old_connections
-
-pymysql.install_as_MySQLdb()
-
-# 取消信号关联，实现数据库长连接
-# signals.request_finished.disconnect(close_old_connections)
